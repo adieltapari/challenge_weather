078174952a11aa73c8749697fb2038c489af46f7
adiel.tapari <adiel.tapari@gmail.com>
2023-01-07 15:50:06 -0300

    add select cities endpoints

--- src/controllers/weather.controller.ts
+++ src/controllers/weather.controller.ts
@@ -1,24 +1,53 @@
 import { Request, Response } from 'express';
 const axios = require('axios');
+import Location from '../models/location';
 
 export const current = async (req: Request, res: Response) => {
-  try {
-    // Instance  Ipapi
-    const instanceIpapi = axios.create({
-      baseURL: `https://ipapi.co/json/`,
-    });
-    const resp = await instanceIpapi.get();
-    const { latitude, longitude, city } = resp.data;
+  const { city } = req.query;
 
-    // Instance  Current Weather
+  try {
+    const respcity = await Location.findOne({ city }).select({ city: 1, lat: 1, lon: 1 });
+
+    if (!respcity) {
+      // Instance  Ipapi
+      const instanceIpapi = axios.create({
+        baseURL: `https://ipapi.co/json/`,
+      });
+      const resp = await instanceIpapi.get();
+      const { latitude, longitude, city } = resp.data;
+
+      // Instance  Current Weather
+      const instanceCurrent = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: {
+          appid: process.env.OPENWEATHER_KEY,
+          units: 'metric',
+          lang: 'es',
+          lat: latitude,
+          lon: longitude,
+        },
+      });
+
+      const respCurrent = await instanceCurrent.get();
+      const { weather, main } = respCurrent.data;
+      return res.status(200).json({
+        Ciudad: city,
+        temp: main.temp,
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        humedad: main.humidity,
+      });
+    }
+    //Current City
     const instanceCurrent = axios.create({
       baseURL: `https://api.openweathermap.org/data/2.5/weather`,
       params: {
         appid: process.env.OPENWEATHER_KEY,
         units: 'metric',
         lang: 'es',
-        lat: latitude,
-        lon: longitude,
+        lat: respcity?.lat,
+        lon: respcity?.lon,
       },
     });
 
@@ -32,6 +61,7 @@ export const current = async (req: Request, res: Response) => {
       max: main.temp_max,
       humedad: main.humidity,
     });
+    return res.status(200).json(respcity);
   } catch (e) {
     return res.status(500).json({
       status: 500,
@@ -41,15 +71,44 @@ export const current = async (req: Request, res: Response) => {
 };
 
 export const forecast = async (req: Request, res: Response) => {
-  try {
-    // Instance  Ipapi
-    const instanceIpapi = axios.create({
-      baseURL: `https://ipapi.co/json/`,
-    });
-    const resp = await instanceIpapi.get();
-    const { latitude, longitude, city } = resp.data;
+  const { city } = req.query;
 
-    // Instance  Forecast Weather
+  try {
+    const respcity = await Location.findOne({ city }).select({ city: 1, lat: 1, lon: 1 });
+
+    if (!respcity) {
+      // Instance  Ipapi
+      const instanceIpapi = axios.create({
+        baseURL: `https://ipapi.co/json/`,
+      });
+      const resp = await instanceIpapi.get();
+      const { latitude, longitude, city } = resp.data;
+
+      // Instance  Forecast Weather
+      const instanceForecast = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/forecast/daily`,
+        params: {
+          appid: process.env.OPENWEATHER_KEY,
+          units: 'metric',
+          lang: 'es',
+          cnt: 5,
+          lat: latitude,
+          lon: longitude,
+        },
+      });
+      const respForecast = await instanceForecast.get();
+      const { list } = respForecast.data;
+
+      return res.status(200).json({
+        Ciudad: city,
+        day1: list[0],
+        day2: list[1],
+        day3: list[2],
+        day4: list[3],
+        day5: list[4],
+      });
+    }
+    // Forecast City
     const instanceForecast = axios.create({
       baseURL: `https://api.openweathermap.org/data/2.5/forecast/daily`,
       params: {
@@ -57,8 +116,8 @@ export const forecast = async (req: Request, res: Response) => {
         units: 'metric',
         lang: 'es',
         cnt: 5,
-        lat: latitude,
-        lon: longitude,
+        lat: respcity?.lat,
+        lon: respcity?.lon,
       },
     });
     const respForecast = await instanceForecast.get();
--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import config from './config/config';
 
+mongoose.set('strictQuery', false);
 mongoose.connect(config.DB.URI);
-
 const connection = mongoose.connection;
 
 connection.once('open', () => {
   console.log('Mongodb connection start');
 });
+mongoose.set('strictQuery', true);
 
 connection.on('error', err => {
   console.log(err);
